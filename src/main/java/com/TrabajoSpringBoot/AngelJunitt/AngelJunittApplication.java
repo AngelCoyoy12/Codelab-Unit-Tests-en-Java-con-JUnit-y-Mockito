4ee0fcdbdad8152524228a3b955ac9990618bdea
Angel Coyoy <furioso7s12@gmail.com>
2024-07-29 12:05:04 -0600

    First Commit

---
+++ src/main/java/com/TrabajoSpringBoot/AngelJunitt/AngelJunittApplication.java
@@ -0,0 +1,13 @@
+package com.TrabajoSpringBoot.AngelJunitt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngelJunittApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngelJunittApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/TrabajoSpringBoot/AngelJunitt/AngelJunittApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TrabajoSpringBoot.AngelJunitt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AngelJunittApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
